aa92a8756820b4ca3fdfc31d9c768ead7b433865
Linus Hochbaum <contact@lhochbaum.de>
2019-03-04 01:09:23 +0100

    Initialize project and create first cog.

---
+++ __main__.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+import json
+from discord.ext import commands
+
+# set description for the bot.
+description = """Ein mechanischer Kerl, welcher ein paar zusätzliche
+Hilfsfunktionen implementiert.
+""" 
+
+# read credentials from creds.json.
+with open("creds.json") as file:
+	creds = json.load(file)
+
+# set up the command bot.
+bot = commands.Bot(command_prefix='!', description=description)
+
+# remove the help command.
+bot.remove_command("help")
+
+# load cogs.
+bot.load_extension("cogs.tlpd")
+
+# finally run the bot!
+bot.run(creds["token"])
---
+++ cogs/tlpd.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+from discord.utils import get
+import asyncio
+
+# emoji ID for :tlpdont:.
+tlpdont = "<:tlpdont:551898126919663636>"
+
+# TLPD cog which contains some trolly stuff.
+class TlpdCog:
+	# initialize the cog.
+	def __init__(self, bot):
+		self.bot = bot
+
+	# a user has sent a message.
+	@asyncio.coroutine
+	def on_message(self, message):
+		# ignore own messages.
+		if message.author == self.bot.user:
+			return
+
+		# lower the message so we can check for substrings easily.
+		lowered = message.content.lower()
+
+		# check for "tlpd" or "zeitverloren" in the message.
+		if "tlpd" in lowered or "zeitverloren" in lowered:
+			# send the emoji.
+			yield from self.bot.send_message(destination=message.channel, content=tlpdont)
+
+def setup(bot):
+	bot.add_cog(TlpdCog(bot))		
